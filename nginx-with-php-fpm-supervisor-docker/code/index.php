6b45b04d3fa189883e051cefe908a91baf1b6d34
arindamgb <arindamgb@gmail.com>
2024-03-27 01:35:54 +0600

    Fixed php files getting downloaded issue when directly accessed

---
+++ nginx-with-php-fpm-supervisor-docker/code/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Results</title>
+</head>
+<body>
+<center>
+    <h1>Results</h1>
+
+    <h2>Version:</h2>
+    <?php include 'version.php'; ?>
+
+    <h2>Maximum File Size:</h2>
+    <?php include 'maxfile.php'; ?>
+
+    <h2>Session:</h2>
+    <?php include 'session.php'; ?>
+
+    <h2>Info:</h2>
+    <?php include 'info.php'; ?>
+</center>
+</body>
+</html>
